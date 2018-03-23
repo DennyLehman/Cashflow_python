1fb2446ced0a671df0e06c6750a7b2c00ecd27d3
Denny <dendenlehman@gmail.com>
2018-03-23 16:39:08 -0500

    added ppa functions

--- cash_flow_functions.py
+++ cash_flow_functions.py
@@ -30,25 +30,29 @@ def get_datatape_csv(filepath):
     df3 = df3.rename(columns=rename_list)
     
 def get_datatape(filepath):
-    list_of_column_names_required = ['Asset Portfolio', 'ID', 'Capital ($)', 'Type',  '%', 'Partner', 'Interconnected', 'Year 1', 'State']
+    list_of_column_names_required = ['Asset Portfolio', 'ID', 'Capital ($)', 'Type', '$/kW', '%', 'Partner', 'Interconnected', 'Year 1', 'State']
     
-    # dtypes = {'ID': 'str', 'Interconnected': 'str'}
-    # df1 = pd.read_excel(filepath, names=list_of_column_names_required)
-    # df1 = pd.read_excel(filepath, header=0, names=['ID','Type'], na_values=['NA'])
     start = time.time()
     
     # df1 = pd.read_excel(filepath, sheetname = 'Active header1', index_col=None, na_values=['NA'], parse_cols="A:D,J,N,T,AG")
-    df1 = pd.read_excel(filepath, sheetname='Active', header=4, index_col=None, na_values=['N/A'], parse_cols="C:F,J,L,P,V,AI,N") #, converters={'Interconnected': pd.to_datetime})
+    df1 = pd.read_excel(filepath, sheetname='Active', header=4, index_col=None, na_values=['N/A'], parse_cols="C:F,J:L,P,V,AI,N") #, converters={'Interconnected': pd.to_datetime})
     print(time.time()-start)
-    # df1 = pd.read_excel(filepath, index_col=None, na_values=['NA'], parse_cols="A:D,J,N,T,AG", dtype=dtypes)
-    # df1 = pd.read_excel(io=filepath, header=None, names=list_of_column_names_required, dtype=dtypes)
-#    list_of_columns = list(df1.columns)
-#    for i in range(0, len(list_of_column_names_required)):
-#        list_of_columns.remove(list_of_column_names_required[i])
-#        
-#    df1 = df1.drop(list_of_columns, axis=1) 
+ 
     
-    df1 = df1.rename(columns= {'Interconnected':'InService Date', 'Type':'Contract Type', 'Capital ($)':'Committed Capital', 'Estimate':'Annual Attribute', '%':'Escalator', 'Year 1':'Recurring Payment', 'Location':'State'})
+    df1 = df1.rename(columns= {'Interconnected':'InService Date', 'Type':'Contract Type', 'Capital ($)':'Committed Capital', 'Estimate':'Annual Attribute', '$/kW':'Power Rate', '%':'Escalator', 'Year 1':'Recurring Payment', 'Location':'State'})
+    return df1
+
+def get_S1_datatape(filepath):
+    list_of_column_names_required = ['Asset Portfolio - Customer', 'System Project: Sunnova System ID', 'Committed Capital', 'Quote: Contract Type', 'Quote: Recurring Payment', 'Quote: Expected Production - Change Order', 'Quote: Solar Rate', 'Quote: Payment Escalator', 'Quote: Installation State', 'InService Date']
+    
+    start = time.time()
+    
+    # df1 = pd.read_excel(filepath, sheetname = 'Active header1', index_col=None, na_values=['NA'], parse_cols="A:D,J,N,T,AG")
+    df1 = pd.read_excel(filepath, sheetname='Active', header=2, index_col=None, na_values=['N/A'], parse_cols="D:F,I,L,O,U,AH,P,Q")
+    print(time.time()-start)
+ 
+    
+    df1 = df1.rename(columns= {'Asset Portfolio - Customer':'Asset Portfolio - Customer','System Project: Sunnova System ID':'ID', 'Committed Capital':'Committed Capital', 'Quote: Contract Type':'Contract Type','InService Date':'InService Date', 'Quote: Expected Production - Change Order':'Annual Attribute', 'Quote: Solar Rate':'Power Rate', 'Quote: Payment Escalator':'Escalator', 'Quote: Recurring Payment':'Recurring Payment', 'Quote: Installation State':'State'})
     return df1
     
 
@@ -63,18 +67,20 @@ def get_contract_type(df1, target_contracts):
     return df1
 
 def remove_non_inService_Systems(df):
+    #df['InService Date'] = pd.to_datetime(df['InService Date'])
     remove = []
-    for i in range(1, df['InService Date'].size):
-        if type(df['InService Date'].iloc[i]) == datetime.time:
+    for i in range(0, df['InService Date'].size):
+        if type(df['InService Date'].iloc[i]) == pd._libs.tslib.NaTType:
             remove.append(i)
     df = df.drop(df.index[remove])
-    df['InService Date'] = pd.to_datetime(df['InService Date'])
     return df
-
+#---------------------------------------------------------------------------
 def remove_blank_inservice_date(df):
     remove = []
     for i in range(1, df['InService Date'].size):
         if df['InService Date'].iloc[i] == datetime.time(0,0):
+            remove.append(i)
+        if type(df['InService Date'].iloc[i]) == pd._libs.tslib.NaTType:
             remove.append(i)
     df = df.drop(df.index[remove])
     df['InService Date'] = pd.to_datetime(df['InService Date'])
@@ -87,13 +93,22 @@ def convert_date_to_EOM(ser):
 
 def create_first_production_first_payment_and_last_payment_for_ppa(df):
     
-    ins_eom = pd.to_datetime(df['InService Date']) + MonthEnd(1)
+    #do start of month, then offset date, then push to month end
+    ins_eom = pd.to_datetime(df['InService Date']) + MonthEnd(0)
     
-    df['First Production Date'] = ins_eom.apply(pd.DateOffset(months=1))
+    #df['First Production Date'] = df['InService Date'] + pd.DateOffset(months=1)
     
-    df['First Payment Date'] = ins_eom.apply(pd.DateOffset(months=2))
+    df['First Production Date'] = ins_eom + pd.DateOffset(months=1) + MonthEnd(0)
+
+    df['First Payment Date'] = ins_eom + pd.DateOffset(months=2) + MonthEnd(0)
     
-    df['Last Payment Date'] = ins_eom.apply(pd.DateOffset(months=301))
+    df['Last Payment Date'] = ins_eom + pd.DateOffset(months=301) + MonthEnd(0)    
+    
+    #df['First Production Date'] = df['InService Date'] + MonthEnd(0) + pd.DateOffset(months=1) + MonthEnd(0)
+    
+    #df['First Payment Date'] = df['InService Date'] + pd.DateOffset(months=1) + MonthEnd(1)
+    
+    #df['Last Payment Date'] = df['InService Date'] + pd.DateOffset(months=1) + MonthEnd(1)
     
 #==============================================================================
 #     # Make first production date
@@ -119,22 +134,24 @@ def create_first_production_first_payment_and_last_payment_for_ppa(df):
     
 def create_first_payment_and_last_payment_date(df):
     # Make first payment date
-    df['First Payment Date'] = df['InService Date'].apply(pd.DateOffset(months=1))
+    df['First Payment Date'] = pd.to_datetime(df['InService Date']) + pd.DateOffset(months=1) + MonthEnd(0)
     
-    # Convert First Payment Date to datetime
-    df['First Payment Date'] = pd.to_datetime(df['First Payment Date']) + MonthEnd(1)
+    # Make Last Payment Date 300 months after the first payment
+    df['Last Payment Date'] = df['First Payment Date'] + pd.DateOffset(months=299) + MonthEnd(0)
     
-    # ********************************************  Rewrite this code **************************************
-    # Make End date
-    df['Last Payment Date'] = df['First Payment Date'].apply(pd.DateOffset(months=299))
-    
-    # Convert End date to date time
-    df['Last Payment Date'] = pd.to_datetime(df['Last Payment Date'])# Make first payment date
     
     return df
 
+def year_diff_300():
+        #return
+    yd = np.zeros(12)
+    for i in range(1,25):
+        yd = np.append(yd, np.ones(12)*i)
+    return yd
+
 def year_diff(first_date, last_date):
     
+        
     #convert last date to timestamp
     last_date = pd.to_datetime(last_date)
     
@@ -146,28 +163,37 @@ def year_diff(first_date, last_date):
     
     return yd
     
-def calculate_production(annual_attribute, Sens, DF, ins_date, curr_date, df_state_curve):
-    df_state_curve = get_state_curve_data(r'C:\Users\denny.lehman\Documents\18_01 Monthly Portfolio Report\Cashflow_Modeling_Python\AP6WII_StateCurves_2018.02.23.xlsx')
-    Sens = 0.975
-    DF = 0.005
-    curr_date = datetime.date(2018,1,1)
-    ins_date = datetime.date(2012,8,1)
-    year_difference = year_diff(ins_date, curr_date)
-    annual_attribute = 5888 
-    state = 'CA'
-    seasonality_curve_factor = get_seasonality_curve(df_state_curve, state, curr_date.month, 1)
+def calculate_production(annual_attribute, Sens, DF, year_difference, seasonality_curve_factor):
+    #df_state_curve = get_state_curve_data(r'C:\Users\denny.lehman\Documents\18_01 Monthly Portfolio Report\Cashflow_Modeling_Python\AP6WII_StateCurves_2018.02.23.xlsx')
+    #Sens = 0.975
+    #DF = 0.005
+    #curr_date = datetime.date(2018,1,1)
+    #ins_date = datetime.date(2012,8,1)
+    #year_difference = year_diff(first_date, curr_date)
+    #annual_attribute = 5888 
+    #state = 'CA'
+    #seasonality_curve_factor = get_seasonality_curve(df_state_curve, state, curr_date.month, 1)
     
-    production = annual_attribute * (1 - Sens) * (1 - DF) ** year_difference * seasonality_curve_factor
+    production = annual_attribute * seasonality_curve_factor * (Sens) * (1 - DF) ** year_difference 
 
 
     return production
 
 # Based on the state and month, return the value of the production factor. This factor
 # will then be multiplied to the annual_attribute year 1 production
-def get_seasonality_curve(df_state_curves, state, month_num, asset_portfolio_factor):
-    ratio_val =  df_state_curves[(df_state_curves['month'] == month_num) & (df_state_curves['state'] == state)]['value'].iloc[0]
+def get_seasonality_curve_single_value(df_state_curve, state, month_num, asset_portfolio_factor):
+    ratio_val =  df_state_curve[(df_state_curve['month'] == month_num) & (df_state_curve['state'] == state)]['value'].iloc[0]
     return ratio_val
+
+def get_seasonality_curve_300(df_state_curve, state, start_month):
     
+    month_values =  df_state_curve[df_state_curve['state']==state]['value']
+    shifted_month_curve = pd.concat([month_values.iloc[start_month-1:],month_values.iloc[:start_month-1]])
+    arr = np.array(shifted_month_curve)
+    for i in range(0,24):
+        arr = np.concatenate([arr,shifted_month_curve])
+    return arr
+
 # function returns the state curve data and does formatting (if needed)
 def get_state_curve_data(filepath):
     return pd.read_excel(filepath)
